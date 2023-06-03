2366b6241444943653f4c98a95f4cf2ad50c3443
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2023-06-03 14:55:17 +0200

    Symfony upload

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 
 use App\Entity\Actor;
+use App\Form\ActorType;
+use App\Form\ProgramType;
 use App\Repository\ActorRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,9 +23,34 @@ class ActorController extends AbstractController
         return $this->render('actor/index.html.twig', ['actors' => $actor]);
     }
 
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, ActorRepository $actorRepository): Response
+    {
+        $actor = new Actor();
+
+        // Create the form, linked with $category
+        $form = $this->createForm(ActorType::class, $actor);
+
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted() && $form->isValid()) {
+            $actorRepository->save($actor, true);
+ 
+            $this->addFlash('success', 'Un nouvel acteur a été ajoutée');
+            // Redirect to categories list
+            return $this->redirectToRoute('actor_index');
+        }
+        
+        // Render the form
+        return $this->render('actor/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
     #[Route('/{name}', name: 'show')]
     public function show(Actor $actor): Response
     {   
         return $this->render('actor/show.html.twig', ['actor' => $actor]);
     }
+
 }
