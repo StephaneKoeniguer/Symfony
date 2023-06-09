1f2bbe4119b344750af55454f4493e26d6303eeb
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2023-06-09 23:41:58 +0200

    inscription utilisateur

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProgramRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/', name:'app_')]
 class DefaultController extends AbstractController
 {
-    #[Route('/', name:'app_index')]
-
-    public function index(): Response
+    #[Route('/', name:'index')]
+    public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('index.html.twig');
+        $programs = $programRepository->findAll();
+        return $this->render('index.html.twig',['programs' => $programs]);
     }
 }
